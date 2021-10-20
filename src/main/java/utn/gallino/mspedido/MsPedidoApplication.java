c1d6e790e4730bcaa1c8c8b4a257bc31801daee5
juangallino <juangallino>
2021-10-20 08:48:54 -0300

    Agregado Eureka

--- src/main/java/utn/gallino/mspedido/MsPedidoApplication.java
+++ src/main/java/utn/gallino/mspedido/MsPedidoApplication.java
@@ -2,8 +2,12 @@ package utn.gallino.mspedido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableJms
 public class MsPedidoApplication {
 
 	public static void main(String[] args) {
